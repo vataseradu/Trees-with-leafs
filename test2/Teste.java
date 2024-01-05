53cd742f236dd4d9690e1fd9302b06457893a3c8
vataseradu <130003640+vataseradu@users.noreply.github.com>
2024-01-05 20:50:08 +0200

    Update Teste.java

--- test2/Teste.java
+++ test2/Teste.java
@@ -9,12 +9,12 @@ public class Teste {
     public static void main(String[] args) {
         Random rand = new Random();
         Copac[] copac = new Copac[6];
-        int i = 0, aux;
+        int i = 0;
         while (i < 6) {
             copac[i] = new Brad(rand.nextInt(2, 10) + 1, rand.nextInt(1, 100) + 1);
             i++;
         }
-        i = 0;
+        //i = 0;
         copac[0].sortareElemente(copac); // SORTARE + COMPARA CU
         System.out.println("------------------------");
         System.out.println("--------TOP BRAZI IN FUNCTIE DE FRUNZE SI VARSTA------- ");
@@ -26,10 +26,9 @@ public class Teste {
         System.out.println("INTRE LOCUL 1 SI LOCUL 2 A FOST O DIFERENTA DE " + (copac[0].getNumarFrunze() - copac[1].getNumarFrunze()) + " FRUNZE");
         System.out.println();
         while (i < 6) {
-            System.out.println("LOCUL " + loc + " --- " + copac[i]);
+            System.out.println("LOCUL " + loc + " --- " + copac[i] + "  // numar frunze="+copac[i].getNumarFrunze()+"  // inaltimea coroanei="+copac[i].getInaltimeCoroana());
             i++;
             loc++;
         }
-
     }
 }
