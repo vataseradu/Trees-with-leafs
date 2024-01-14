f093cca770a19d49bd7feb8c9adfc8e542eda9b6
vataseradu <130003640+vataseradu@users.noreply.github.com>
2024-01-14 23:34:04 +0200

    Update Copac.java

--- test2/forma1/Copac.java
+++ test2/forma1/Copac.java
@@ -1,16 +1,26 @@
 package test2.forma1;
 
 public abstract class Copac {
-    public Copac(){
+    int varsta;
 
+    public Copac(int varsta) {
+        this.varsta = varsta;
     }
-    public abstract int comparaCu(Copac deComparat);
+
+    public Copac() {
+    }
+
+    public abstract int comparaCu(Copac copac);
+
     public abstract void afiseaza();
-    public abstract int getNumarFrunze();
-    public abstract int getInaltimeCoroana();
+
     public abstract int getVarsta();
-    public abstract String toString();
+
     public abstract void sortareElemente(Copac[] copac);
+
+    public abstract int getInaltimeCoroana();
+
+    public abstract int getNumarFrunze();
 
 
 }
